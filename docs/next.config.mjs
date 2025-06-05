5be90f24f3e194b10ff6a83a09f3e658f381cdb4
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-05 15:02:23 +0300

    feat(docs): add redirect to /start page

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,10 +1,14 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
+import { redirectsConfig } from "./redirects.config.mjs";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return redirectsConfig;
+  }
 };
 
 export default withMDX(config);
