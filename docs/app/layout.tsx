4857149fca9ecb5c6a934c715ee4bd674ad57150
Timofey.Seryakov <t.seriakov@gmail.com>
2025-06-07 13:17:45 +0300

    feat(docs): migrate Getting Started chapters

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,6 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
 import { baseOptions } from './layout.config';
 import { source } from '@/lib/source';
 import type { ReactNode } from 'react';
@@ -10,11 +10,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <body>
         <RootProvider>
-          <DocsLayout tree={source.pageTree} {...baseOptions}>
+          <DocsLayout
+            tree={source.pageTree}
+            {...baseOptions}
+          >
             {children}
           </DocsLayout>
         </RootProvider>
       </body>
-    </html>
+    </html >
   );
 }
--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,10 +1,22 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import * as FilesComponents from 'fumadocs-ui/components/files';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { Callout } from './app/_components/Callout';
+import { StackBlitz } from './app/_components/StackBlitz';
+import { Icon } from '@iconify/react'
 
-// use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
+    ...FilesComponents,
     ...components,
+    Accordion,
+    Accordions,
+    Callout,
+    StackBlitz,
+    Icon
   };
 }
