7858f00903bfe43e146d470d42edf0fe3f540b49
Timofei.Seriakov <t.seriakov@gmail.com>
2025-06-21 19:42:43 +0300

    feat(search): add static search functionality

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,37 +1,41 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { DocsLayout } from 'fumadocs-ui/layouts/notebook';
-import { baseOptions } from './layout.config';
-import { source } from '@/lib/source';
-import { GithubInfo } from 'fumadocs-ui/components/github-info';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { DocsLayout } from "fumadocs-ui/layouts/notebook";
+import { baseOptions } from "./layout.config";
+import { source } from "@/lib/source";
+import { GithubInfo } from "fumadocs-ui/components/github-info";
+import type { ReactNode } from "react";
+import { Provider } from "./provider";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <RootProvider>
+        <Provider>
           <DocsLayout
             tree={source.pageTree}
-            // nav={{ ...baseOptions.nav, mode: 'top' }}
             nav={{ ...baseOptions.nav }}
             tabMode="navbar"
             links={[
               {
-                text: 'Discord',
-                url: 'https://discord.gg/952SFk8Tb8',
+                text: "Discord",
+                url: "https://discord.gg/952SFk8Tb8",
               },
               {
-                type: 'custom',
+                type: "custom",
                 children: (
-                  <GithubInfo owner="bknd-io" repo="bknd" className="lg:-mx-2" />
+                  <GithubInfo
+                    owner="bknd-io"
+                    repo="bknd"
+                    className="lg:-mx-2"
+                    token={process.env.GITHUB_TOKEN}
+                  />
                 ),
               },
             ]}
           >
             {children}
           </DocsLayout>
-        </RootProvider>
+        </Provider>
       </body>
     </html>
   );
