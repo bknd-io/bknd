a9efee35553298f73332c36d4842ac0c021bf409
dswbx <dennis.senn@gmx.ch>
2025-07-09 08:13:42 +0200

    initial waku

---
+++ examples/waku/bknd-types.d.ts
@@ -0,0 +1,15 @@
+import type { DB } from "bknd/core";
+import type { Insertable, Selectable, Updateable, Generated } from "kysely";
+
+declare global {
+  type BkndEntity<T extends keyof DB> = Selectable<DB[T]>;
+  type BkndEntityCreate<T extends keyof DB> = Insertable<DB[T]>;
+  type BkndEntityUpdate<T extends keyof DB> = Updateable<DB[T]>;
+}
+
+interface Database {
+}
+
+declare module "bknd/core" {
+  interface DB extends Database {}
+}
