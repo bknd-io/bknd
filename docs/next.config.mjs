5981ff143e7c11d1e44d1f47caf04d075d988007
dswbx <dennis.senn@gmx.ch>
2025-07-24 09:27:12 +0200

    docs: adjusted auto type table, added llm functions

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,8 +1,8 @@
 import { createMDX } from "fumadocs-mdx/next";
 import { redirectsConfig } from "./redirects.config.mjs";
 import { rewritesConfig } from "./rewrites.config.mjs";
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,22 +11,22 @@ const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  output: "export",
-  trailingSlash: true,
-  reactStrictMode: true,
-  serverExternalPackages: ["typescript", "twoslash"],
+   output: "export",
+   trailingSlash: true,
+   reactStrictMode: true,
+   serverExternalPackages: ["typescript", "twoslash"],
 
-  webpack(config) {
-    config.resolve.alias["@/bknd"] = path.resolve(__dirname, "../app/src");
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
+   webpack(config) {
+      config.resolve.alias["@/bknd"] = path.resolve(__dirname, "../app/src");
+      config.resolve.alias["@"] = path.resolve(__dirname);
+      return config;
+   },
 };
 
 if (process.env.NODE_ENV === "development") {
-  config.output = "standalone";
-  config.redirects = async () => redirectsConfig;
-  config.rewrites = async () => rewritesConfig;
+   config.output = "standalone";
+   config.redirects = async () => redirectsConfig;
+   config.rewrites = async () => rewritesConfig;
 }
 
 export default withMDX(config);
--- docs/rewrites.config.mjs
+++ docs/rewrites.config.mjs
@@ -1,1 +1,6 @@
-export const rewritesConfig = [];
+export const rewritesConfig = [
+   {
+      source: "/:path*.mdx",
+      destination: "/llms.mdx/:path*",
+   },
+];
