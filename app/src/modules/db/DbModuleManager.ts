ff56d616d90fcb2aa434628dbc60694ceb835b37
dswbx <dennis.senn@gmx.ch>
2025-11-11 15:57:24 +0100

    chore: upgrade bun to 1.3.2

--- app/src/modules/db/DbModuleManager.ts
+++ app/src/modules/db/DbModuleManager.ts
@@ -290,13 +290,12 @@ export class DbModuleManager extends ModuleManager {
                   updated_at: new Date(),
                });
             }
-         } else if (e instanceof TransformPersistFailedException) {
-            $console.error("ModuleManager: Cannot save invalid config");
-            this.revertModules();
-            throw e;
          } else {
+            if (e instanceof TransformPersistFailedException) {
+               $console.error("ModuleManager: Cannot save invalid config");
+            }
             $console.error("ModuleManager: Aborting");
-            this.revertModules();
+            await this.revertModules();
             throw e;
          }
       }
