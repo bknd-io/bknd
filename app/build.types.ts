10c65a8d0e5fd4ad5b78cae68065a6662a674181
Jonas Perusquia Morales <jonaspm@enerlinq.com>
2026-04-01 18:14:43 -0600

    chore: add entry point to buildTypes function and enable declaration in tsconfig

--- app/build.types.ts
+++ app/build.types.ts
@@ -2,30 +2,32 @@ import { build } from "tsdown";
 import c from "picocolors";
 
 export async function buildTypes() {
-    await build({
-        tsconfig: "tsconfig.build.json",
-        watch: false,
-        clean: true,
-        logLevel: "silent",
-        unbundle: true,
-        root: "src",
-        outDir: "dist/types",
-        dts: {
-          emitDtsOnly: true,
-          resolver: "oxc"
-        },
-        deps: {
-         skipNodeModulesBundle: true
-        }
-    });
+  await build({
+    tsconfig: "tsconfig.build.json",
+    watch: false,
+    clean: true,
+    logLevel: "silent",
+    unbundle: true,
+    root: "src",
+    outDir: "dist/types",
+    entry: ["./src/**/*.ts", "./src/**/*.tsx"],
+    dts: {
+      emitDtsOnly: true,
+      resolver: "oxc",
+      eager: true
+    },
+    deps: {
+      skipNodeModulesBundle: true
+    },
+  });
 }
 
 if (import.meta.main) {
-   try {
-      await buildTypes();
-      console.log(c.cyan("[Types]"), c.green("built"));
-   } catch (error) {
-      console.error(c.cyan("[Types]"), c.red("build failed"), error);
-      process.exitCode = 1;
-   }
+  try {
+    await buildTypes();
+    console.log(c.cyan("[Types]"), c.green("built"));
+  } catch (error) {
+    console.error(c.cyan("[Types]"), c.red("build failed"), error);
+    process.exitCode = 1;
+  }
 }
